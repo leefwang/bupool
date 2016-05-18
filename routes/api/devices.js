9eb5c1b12899d304a92a696cdedc0813b9655d72
Sang Yun Lee <lsywind3@gmail.com>
2016-05-19 00:39:30 +0900

    push demo 추가

--- routes/api/devices.js
+++ routes/api/devices.js
@@ -57,4 +57,37 @@ router.all('/put', function(req, res, next) {
   });
 });
 
+router.all('/push', function(req, res, next) {
+  var gcm = require('node-gcm');
+
+// create a message with default values
+  var message = new gcm.Message();
+
+// or with object values
+  var message = new gcm.Message({
+    collapseKey: 'demo',
+    delayWhileIdle: true,
+    timeToLive: 3,
+    data: {
+      key1: '안녕하세요.',
+      key2: 'saltfactory push demo'
+    }
+  });
+
+  var server_access_key = '푸시 프로바이더 서버 access key 값';
+  var sender = new gcm.Sender(server_access_key);
+  var registrationIds = [];
+
+  var registration_id = '안드로이드 registration_id 값';
+// At least one required
+  registrationIds.push(registration_id);
+
+  /**
+   * Params: message-literal, registrationIds-array, No. of retries, callback-function
+   **/
+  sender.send(message, registrationIds, 4, function (err, result) {
+    console.log(result);
+  });
+});
+
 module.exports = router;
