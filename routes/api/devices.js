3d587abae068d100758f0c63dc1c56451e868ed9
Sang Yun Lee <lsywind3@gmail.com>
2016-05-19 04:11:34 +0900

    udid가 등록되어 있는 경우 device table을 update

--- routes/api/devices.js
+++ routes/api/devices.js
@@ -3,22 +3,34 @@ var express = require('express');
 var router = express.Router();
 
 router.all('/put', function(req, res, next) {
-  models.devices.findOne({
+  models.users.findOne({
     where: {
-      device: req.body.device
+      email: req.body.email
     }
-  }).then(function (device) {
-    if (device) {
-      return res.json({
-        result: 2
-      })
-    } else {
-      models.users.findOne({
+  }).then(function (user) {
+    if (user) {
+      models.devices.findOne({
         where: {
-          email: req.body.email
+          device: req.body.device
         }
-      }).then(function (user) {
-        if (user) {
+      }).then(function (device) {
+        if (device) {
+          device.update({
+            user_id: user.id,
+            device: req.body.device,
+            token: req.body.deviceToken,
+            platform: req.body.platform
+          }, {fields: ['user_id', 'device', 'token', 'platform']}).then(function() {
+            return res.json({
+              result: 2,
+              device: device
+            })
+          }).catch(function() {
+            return res.json({
+              result: 0
+            })
+          });
+        } else {
           var device = models.devices.build({
             user_id: user.id,
             device: req.body.device,
@@ -26,28 +38,23 @@ router.all('/put', function(req, res, next) {
             platform: req.body.platform
           });
 
-          device.save().then(function() {
+          device.save().then(function () {
             return res.json({
               result: 1,
               device: device
             });
-          }).catch(function(err) {
+          }).catch(function (err) {
             return res.json({
               result: 0,
               err: err
             });
           });
-        } else {
-          return res.json({
-            result: 3
-          })
         }
-      }).catch(function(err) {
-        return res.json({
-          result: 0,
-          err: err
-        });
       });
+    } else {
+      return res.json({
+        result: 3
+      })
     }
   }).catch(function(err) {
     return res.json({
