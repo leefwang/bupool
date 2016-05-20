956bbaf3ad229921fbf6e26bd954c2ed5460b6fa
Sang Yun Lee <lsywind3@gmail.com>
2016-05-20 11:50:23 +0900

    push send 기능 및 web page 구현

--- routes/api/push.js
+++ routes/api/push.js
@@ -1,28 +1,55 @@
+var models = require('../../models');
+var express = require('express');
+var router = express.Router();
 
-
-router.all('/push', function(req, res, next) {
-  var gcm = require('node-gcm');
-  var fs = require('fs');
-
-  var message = new gcm.Message({
-    collapseKey: 'demo',
-    delayWhileIdle: true,
-    timeToLive: 3,
-    data: {
-      title: 'saltfactory GCM demo',
-      message: 'Google Cloud Messaging 테스트'
+router.all('/send', function(req, res, next) {
+  models.push_messages.findOne({
+    include: [{ all: true }],
+    where : {
+      id: req.query.id
     }
+  }).then(function (pushMessages) {
+    models.devices.findAll({
+
+    }).then(function (devices) {
+      var gcm = require('node-gcm');
+      var fs = require('fs');
+
+      var message = new gcm.Message({
+        collapseKey: 'demo',
+        delayWhileIdle: true,
+        timeToLive: 3,
+        data: {
+          title: push_messages.title,
+          message: push_messages.message
+        }
+      });
+
+      var server_api_key = 'AIzaSyCU71ywHf21rf1QXGjcaBzKO5TKKNr96fw';
+      var sender = new gcm.Sender(server_api_key);
+      var registrationIds = [];
+
+      for (var i = 0; i < devices.length; i++) {
+        registrationIds.push(devices[i].token);
+      }
+
+      sender.send(message, registrationIds, 4, function (err, result) {
+        console.log(result);
+      });
+    });
+  });
+});
+
+router.all('/add', function(req, res, next) {
+  var push = models.push_messages.build({
+    title: req.query.title,
+    messages: req.query.messages
   });
 
-  var server_api_key = 'AIzaSyCU71ywHf21rf1QXGjcaBzKO5TKKNr96fw';
-  var sender = new gcm.Sender(server_api_key);
-  var registrationIds = [];
+  push.save().then(function() {
 
-  var token = 'cl_Mt8IHU9U:APA91bE9fCOc8tXNbiur1Nqe5lbzUNglkfkHsae8bJylBxCGE8pDGbXp6tl8x_fQfvX7VyjXnwc2FPdlVqqWXG8bWBOUy89IyoXsGqE2BL_kuv2lEZ8wPg_joGxKxh5Xak3UtZHt_sHo';
-  registrationIds.push(token);
+  }).catch(function() {
 
-  sender.send(message, registrationIds, 4, function (err, result) {
-    console.log(result);
   });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/push', function(req, res, next) {
+  models.push_messages.findAll({
+    include: [{ all: true }]
+  }).then(function (pushMessages) {
+    res.render('push-send', {pushMessages: pushMessages});
+  });
 });
 
 module.exports = router;
